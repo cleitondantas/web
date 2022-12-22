2f82ae6b590fe14b9f933a48dcfabacc5df0f137
Cleiton Dantas dos Anjos <cleiton.dantas@outlook.com>
2022-12-22 11:35:32 -0300

    novo endpoint

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -1,12 +1,15 @@
 package com.example.web;
 
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 
 @SpringBootApplication
@@ -16,15 +19,26 @@ public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
-
+    public Long valueRandom= 0L;
+    public Long contRequests= 0L;
     Logger logger = LoggerFactory.getLogger(WebApplication.class);
 
+    @PostConstruct
+    public void init(){
+        valueRandom = new Random().nextLong();
+    }
     @GetMapping("/")
     public String status(){
         logger.info("------UP-----");
         return "UP";
     }
 
+    @GetMapping("/pod")
+    public String random(){
+        contRequests++;
+        logger.info("pod: "+this.valueRandom +" Cont Requests: "+contRequests);
+        return "pod: "+this.valueRandom;
+    }
 }
 
 
